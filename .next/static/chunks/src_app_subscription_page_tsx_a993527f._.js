f5140f53df9ca99d96c24a795016644095dba0cd
PeriodCarePackage <leogptpremium@gmail.com>
2025-07-23 15:18:18 +0800

    Add .next/static/chunks/src_app_subscription_page_tsx_a993527f._.js 1753255098204

---
+++ .next/static/chunks/src_app_subscription_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_be5733b9._.js",
+  "static/chunks/src_81e15093._.js"
+],
+    source: "dynamic"
+});
